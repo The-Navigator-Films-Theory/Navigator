970e440eee960b9c7064c1870fcae92194cb68e7
JAY CHOKSI <jaychokxii1804@gmail.com>
2026-04-07 19:46:54 -0400

    fix: Correct formatting of externalLinks configuration

--- src/config/externalLinks.ts
+++ src/config/externalLinks.ts
@@ -1,9 +1,8 @@
 export const EXTERNAL_LINKS = {
-  betaFeedbackFormUrl: 'https://docs.google.com/forms/d/e/1FAIpQLSevHjWyq6pLK4NSxz68b-AxDMqS9t8q1QiPWipgn8n1N8eV6w/viewform?usp=publish-editor'
-//   betaQaFormUrl: '',
-// 
-}
-;
+  betaFeedbackFormUrl:
+    'https://docs.google.com/forms/d/e/1FAIpQLSevHjWyq6pLK4NSxz68b-AxDMqS9t8q1QiPWipgn8n1N8eV6w/viewform?usp=publish-editor',
+  betaQaFormUrl: '',
+};
 
 export function hasExternalLink(url: string): boolean {
   return Boolean(url && url.trim().length > 0);
